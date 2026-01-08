a94ecdc6a635c97530f25163e386fd16006104ce
Marcelo Jung <marcelo_4940@hotmail.com>
2026-01-07 21:20:59 -0300

    vai que vai

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
+import { useState } from 'react';
+import type { TaskStateModel } from './models/TaskStateModel';
 import { Home } from './pages/Home';
 import './styles/global.css';
 import './styles/theme.css';
 
-function App() {
+const initialState: TaskStateModel = {
+  tasks: [],
+  secondsRemaining: 0,
+  formattedSecondsRemaining: '00:00',
+  activeTask: null,
+  currentCycle: 0,
+  config: {
+    workTime: 25,
+    shortBreakTime: 5,
+    longBreakTime: 15,
+  },
+};
 
-  return <Home />;
+function App() {
+  const [state, setState] = useState(initialState);
+
+  return <Home state={state} setState={setState} />;
 }
 
 export default App
