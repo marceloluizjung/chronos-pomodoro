6d14c1800db630a65d59796933249aca645c34e7
unknown <marcelo_4940@hotmail.com>
2026-01-11 21:59:35 -0300

    vai que vai

--- eslint.config.js
+++ eslint.config.js
@@ -15,6 +15,10 @@ export default defineConfig([
       reactHooks.configs['recommended-latest'],
       reactRefresh.configs.vite,
     ],
+    rules: {
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': 'off'
+    },
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,9 @@
-import { useState } from 'react';
-import { TaskContextProvider } from './contexts/TaskContext';
-import type { TaskStateModel } from './models/TaskStateModel';
+import { TaskContextProvider } from './contexts/TaskContext/TaskContextProvider';
 import { Home } from './pages/Home';
 import './styles/global.css';
 import './styles/theme.css';
 
-const initialState: TaskStateModel = {
-  tasks: [],
-  secondsRemaining: 0,
-  formattedSecondsRemaining: '00:00',
-  activeTask: null,
-  currentCycle: 0,
-  config: {
-    workTime: 25,
-    shortBreakTime: 5,
-    longBreakTime: 15,
-  },
-};
-
 function App() {
-  const [state, setState] = useState(initialState);
-
   return (
     <TaskContextProvider>
       <Home />
