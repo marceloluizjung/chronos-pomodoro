303e6c9563477755922622ab1daf84400c936e1a
Marcelo Jung <marcelo_4940@hotmail.com>
2026-01-13 14:31:55 -0300

    vai que vai

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -1,14 +1,53 @@
 import { PlayCircleIcon } from "lucide-react";
 import { useRef } from "react";
+import { useTaskContext } from "../../contexts/TaskContext/useTaskContext";
+import type { TaskModel } from "../../models/TaskModel";
+import { getNextCycle } from "../../utils/getNextCycle";
+import { getNextCycleType } from "../../utils/getNextCycleType";
 import { Cycles } from "../Cycles";
 import { DefaultButton } from "../DefaultButton";
 import { DefaultInput } from "../DefaultInput";
 
 export function MainForm() {
+    const { state, setState } = useTaskContext();
     const taskNameInput = useRef<HTMLInputElement>(null);
+
+    const nextCycle = getNextCycle(state.currentCycle);
+    const nextCycleType = getNextCycleType(nextCycle);
 
     function handleCreateNewTask(event: React.FormEvent<HTMLFormElement>) {
         event.preventDefault();
+
+        if (taskNameInput.current === null) return;
+
+        const taskName = taskNameInput.current.value.trim();
+
+        if (!taskName) {
+            alert('Digite o nome da tarefa');
+            return;
+        }
+
+        const newTask: TaskModel = {
+            id: Date.now().toString(),
+            name: taskName,
+            startDate: Date.now(),
+            completeDate: null,
+            interruptDate: null,
+            duration: state.config[nextCycleType],
+            type: nextCycleType
+        };
+
+        const secondsRemaining = newTask.duration * 60;
+
+        setState(prevState => ({
+            ...prevState,
+            config: { ...prevState.config },
+            activeTask: newTask,
+            currentCycle: 1, // Conferir
+            secondsRemaining,
+            formattedSecondsRemaining: '00:00', // Conferir
+            tasks: [...prevState.tasks, newTask]
+        }));
     }
 
     return (
