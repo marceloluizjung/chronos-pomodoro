3517e9187935b823c6a311e97df050024b031fe8
unknown <marcelo_4940@hotmail.com>
2026-02-10 22:02:32 -0300

    Vai que vai

--- src/contexts/AuthContext/AuthContextStorage.ts
+++ src/contexts/AuthContext/AuthContextStorage.ts
@@ -2,5 +2,10 @@ import { authContextInitialState } from "./AuthContextInitialState";
 
 export function authContextStorage() {
     const storageValues = localStorage.getItem("chronos-pomodoro-login");
-    return storageValues ? JSON.parse(storageValues) : authContextInitialState;
+    try {
+        return storageValues ? JSON.parse(storageValues) : authContextInitialState;
+    }
+    catch {
+        return authContextInitialState;
+    }
 }
